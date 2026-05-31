2292df79a7ab8e4adf6756cf04253a7e7b9d365c
Abi Akindele <2366595+abbeymart@users.noreply.github.com>
2026-05-31 16:14:32 -0400

    updated-deps-and-comments

--- src/stdResMessages.ts
+++ src/stdResMessages.ts
@@ -1,6 +1,6 @@
 /**
  * @Author: abbeymart | Abi Akindele | @Created: 2020-07-11
- * @Company: Copyright 2020 Abi Akindele  | mConnect.biz
+ * @Company: Copyright 2020 Abi Akindele | mConnect.biz
  * @License: All Rights Reserved | LICENSE.md
  * @Description: @mconnect/res-messages, response-messages | settings, default values
  */
